620b2dd9adb47197f057611a694866efb0a9a065
thereaper90917 <42868546+thereaper90917@users.noreply.github.com>
2020-08-11 17:38:50 +0200

    added cor support

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restless import APIManager
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 db = SQLAlchemy(app)
 manager = APIManager(app,flask_sqlalchemy_db=db)
 
@@ -15,10 +17,13 @@ class Dev(db.Model):
     description = db.Column(db.String(255),nullable=False)
     image_url = db.Column(db.String(255), nullable=False)
 
-
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(80),nullable=False)
 
 
 manager.create_api(Dev, methods=['GET', 'POST'])
+manager.create_api(User, methods=['GET'])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
